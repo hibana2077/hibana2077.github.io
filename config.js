fbaec9086b12ae8ff0a19db164f7d0186c04ab8d
hibana2077 <hibana2077@gmaill.com>
2025-04-01 12:59:19 +0800

    No code changes made.

--- config.js
+++ config.js
@@ -1,302 +1,216 @@
 module.exports = {
-	avatar: 'https://avatars.githubusercontent.com/u/72302793?size=128',
-	pathPrefix: '',
-	tocMaxDepth: 2,
-	excerptMaxLength: 500,
-	postsForArchivePage: 3,
-	siteUrl: 'https://hibana2077.com',
-	title: 'Syuan-Hao Li (Tom)',
-	description: 'Syuan-Hao Li\'s website.',
-	author: 'Syuan-Hao Li',
-	authorAlternative: '李軒豪',
-	// birthday: 'Mar. 10, 2003',
-	location: 'Taipei, Taiwan',
-	email: 'hibana2077@gmail.com',
-	pages: {
-		home: '/',
-		posts: 'posts',
-		contact: 'contact',
-		resume: 'resume',
-		tags: 'tags',
-		research: 'research',
-	},
-	professions: [
-		'Graduate Student',
-	],
-	introduction: [
-		'I am a B.Sc. student in Computer Science at National Taitung University, Taiwan.'
-	],
-	education: [
-		{
+  pathPrefix: 'https://avatars.githubusercontent.com/u/72302793?size=128',
+  siteUrl: 'https://hibana2077.github.io',
+  title: 'Syuan-Hao Li',
+  description: 'Personal Website of Syuan-Hao Li',
+  author: 'Syuan-Hao Li',
+  authorAlternative: 'Tom',
+  introduction: [
+    'I am a B.Sc. student in Computer Science at National Taitung University, Taiwan.'
+  ],
+  avatar: '',
+  professions: [
+    'Computer Science Student',
+  ],
+  tocMaxDepth: 2,
+  excerptMaxLength: 500,
+  birthday: 'Some day',
+  location: 'Earth',
+  email: 'john.doe@gmail.com',
+  postsForArchivePage: 3,
+  defaultLanguage: 'en',
+  disqusScript: process.env.DISQUS_SCRIPT
+    || 'https://tc-imba.disqus.com/embed.js',
+  pages: {
+    home: '/',
+    posts: 'posts',
+    contact: 'contact',
+    resume: 'resume',
+    tags: 'tags',
+    research: 'research',
+  },
+  social: [
+    {
+      url: '/resume.pdf',
+      icon: ['ai', 'cv'],
+    }, {
+      url: 'https://github.com/',
+      icon: ['fab', 'github'],
+    }, {
+      url: 'https://twitter.com/',
+      icon: ['fab', 'twitter'],
+    }, {
+      url: 'https://scholar.google.com/',
+      icon: ['fab', 'google-scholar'],
+    },
+  ],
+  // facebook: 'https://www.facebook.com/rolwin.monteiro',
+  // instagram: 'https://www.instagram.com/reevan100/',
+  // rss: '/rss.xml',
+  wakatime: {
+    username: 'tcimba',
+    activity: '7add4047-08f9-4da8-b649-aa114503678f',
+    language: '460a84ab-722a-4b80-b896-cabaa13ad7eb',
+    editor: 'd851639a-28d8-4884-949f-d338a858f7e9',
+    os: 'caf7d0d1-8fd2-4595-a991-363c8583fea9',
+  },
+  contactFormUrl: process.env.CONTACT_FORM_ENDPOINT
+    || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
+  googleAnalyticTrackingId: process.env.GA_TRACKING_ID || 'G-ZK3P43DY6M',
+  education: [
+    {
       date: 'Jan 2020 - Present',
       icon: 'graduation-cap',
       title: 'Ph.D in Computer Science',
       location: 'Some University, Somewhere',
     }, {
-			date: 'Sep. 2021 - May. 2025',
-			icon: 'university',
-			title: 'B.Sc. in Computer Science',
-			location: 'National Taitung University',
-		}
-	],
-	social: [
-		{
-			url: '/cv.pdf',
-			icon: ['ai', 'cv'],
-		},{
-			url: 'https://github.com/hibana2077',
-			icon: ['fab', 'github'],
-		}, {
-			url: 'https://www.linkedin.com/in/syuan-hao-li-92a860225/',
-			icon: ['fab', 'linkedin'],
-		}
-	],
-	awards: [
-	],
-	interests: [
-		{
-			icon: 'chart-line',
-			title: 'Mathematical Statistics',
-		}, {
-			icon: 'brain',
-			title: 'Learning Theory',
-		}, {
-      title: 'Computer Vision',
-      icon: 'camera',
+      date: 'Sept 2015 - Aug 2019',
+      icon: 'university',
+      title: 'B.S.E. in Electrical and Computer Engineering',
+      location: 'Some University, Somewhere',
     }, {
-      title: 'Natural Language Processing',
-      icon: 'language',
-    }
-	],
-	experience: [
-		{
-			title: 'Teaching',
-			position: 'left',
-			data: [
-				{
-					title: 'Graduate Teaching Assistant',
-					date: 'Jan. 2025 - May 2025',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						'**[IS527 Network Analysis](https://ischool.illinois.edu/degrees-programs/courses/is527)** (Graduate): ' +
-						'An introduction to the theory and applications of social network analysis on the M.S. IS track.',
-				},
-				{
-					title: 'Instructional Aide',
-					date: 'Jan. 2023 - May 2023',
-					location: 'University of Michigan',
-					description:
-						'**[EECS475 Introduction to Cryptography](https://mahdi.ch/eecs475/)** (Undergraduate): ' +
-						'An introduction to the theoretical cryptography on the B.Sc. CS track. [Notes](../posts/Notes#introduction-to-cryptography-eecs475-university-of-michigan-ta) are available.',
-				},
-				{
-					title: 'Instructional Aide',
-					date: 'Aug. 2022 - Dec. 2022',
-					location: 'University of Michigan',
-					description:
-						'**[EECS572 Randomness and Computation](https://mahdi.ch/eecs572/)** (Graduate): ' +
-						'A course about randomness in modern computations on the M.S. CS theory track. [Notes](../posts/Notes#randomness-and-computation-eecs572-university-of-michigan-ta) are available.',
-				},
-				{
-					date: 'May 2021 - Aug. 2021',
-					title: 'Teaching Assistant',
-					location: 'Shanghai Jiao Tong University',
-					description:
-						'**[VV285 Honor Mathematics III](https://umji.sjtu.edu.cn/~horst/teaching/vv285.html)** (Undergraduate): ' +
-						'A follow-up course of VV186 on the B.Eng. ECE track. We also hold the first [*JIntegration Bee* competition](https://github.com/sleepymalc/JIntegrationBee)!',
-				},
-				{
-					title: 'Teaching Assistant',
-					date: 'Sep. 2020 - Dec. 2020',
-					location: 'Shanghai Jiao Tong University',
-					description:
-						'**[VV186 Honor Mathematics II](https://umji.sjtu.edu.cn/~horst/teaching/vv186.html)** (Undergraduate): ' +
-						'The first rigorous analysis course on the B.Eng. ECE track.',
-				},
-			],
-		},
-		{
-			title: 'Professional Service',
-			position: 'left',
-			data: [
-				{
-					title: 'Conference Reviewer',
-					date: 'NA',
-					location: 'Virtual',
-					description:
-						'IEEE BigData *2023*, ICML *2024*, ICLR *2025*',
-				},
-				{
-					title: 'Program Committee',
-					date: 'NA',
-					location: 'Virtual',
-					description:
-						'AAAI *2025*',
-				},
-			],
-		},
-		{
-			title: 'Software Development',
-			position: 'left',
-			data: [
-				{
-					title: 'Backend Developer',
-					date: 'Oct. 2020 - June 2021',
-					location: 'Virtual',
-					description:
-						'**foodall**: ' +
-						'A food order platform in Taiwan, built with *Golang*, *Gin*, and *MongoDB*, with RESTful API design.',
-				},
-			],
-		},
-		{
-			title: 'Reading Group',
-			position: 'right',
-			data: [
-				{
-					title: 'Organizer',
-					date: 'June 2024 - Present',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						`**Data Attribution Reading Group**: ` +
-						`Focus on the recent advances of data attribution methods in the era of Generative AI. Please see the [official website](https://trais-lab.github.io/dattri-reading-group/).`,
-				},
-				{
-					title: 'Organizer',
-					date: 'Jan. 2024 - Present',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						`**Statistical Learning Theory**: ` +
-						`A thorough reading on [Roman Vershynin](https://www.math.uci.edu/~rvershyn/)'s [High-Dimensional Probability](https://www.math.uci.edu/~rvershyn/papers/HDP-book/HDP-book.html), focusing on statistical learning theory. [Solution](../else/HDP_solution.pdf) is available.`,
-				},
-				{
-					title: 'Presenter',
-					date: 'Oct. 2023 - Jul. 2024',
-					location: 'Virtual',
-					description:
-						`**[VIDA Reading Group](https://wanghh7.github.io/VIDY-paper-reading-group/)**: ` +
-						`A reading group focuses on recent advances in graph machine learning, and also the foundational theory for statistical learning.`,
-				},
-				{
-					title: 'Organizer',
-					date: 'May 2023 - Dec. 2023',
-					location: 'University of Illinois Urbana-Champaign',
-					description:
-						`**Sparse Graph Limit Theory**: ` +
-						`A thorough reading on [László Lovász](https://en.wikipedia.org/wiki/L%C3%A1szl%C3%B3_Lov%C3%A1sz)'s [Large networks and graph limits](https://lovasz.web.elte.hu//bookxx/hombook-almost.final.pdf), focusing on Chapter 18 and onwards.`,
-				},
-			],
-		},
-		{
-			title: 'Research and Industry Experience',
-			position: 'right',
-			data: [
-				{
-					title: 'Applied Scientist Intern',
-					date: 'May 2025 - Aug. 2025',
-					location: 'Amazon AWS AI Lab',
-					description:
-						'**Data Curation**: ' +
-						'TBA.'
-				},
-				{
-					title: 'Research Intern',
-					date: 'May 2024 - Aug. 2024',
-					location: 'National Institute of Informatics',
-					description:
-						'[**Pseudo-Non-Linear Data Augmentation**](../research/Pseudo-Non-Linear-Data-Augmentation-via-Energy-Minimization/): ' +
-						'Develop an interpretable and information-geometric data augmentation algorithm advised by [Mahito Sugiyama](https://mahito.info/index_e.html). ' +
-						'Won the **Excellent Internship Evaluation**.',
-				},
-				{
-					title: 'Undergraduate Researcher',
-					date: 'Mar. 2022 - Dec. 2022',
-					location: 'University of Michigan',
-					description:
-						'**Detection Sets**: ' +
-						'Design the first almost linear time algorithm on finding minimal balanced cuts advised by [Thatchaphol Saranurak](https://sites.google.com/site/thsaranurak/).',
-				},
-				{
-					title: 'Undergraduate Researcher',
-					date: 'May 2022 - Apr. 2023',
-					location: 'University of Michigan',
-					description:
-						'**Deep Learning Foundation**: ' +
-						'Develop various techniques to understand deep neural networks advised by [Wei Hu](https://weihu.me/) under the [SURE Program](https://sure.engin.umich.edu/).',
-				},
-			],
-		},
-	],
-	tagColors: [
-		'magenta', 'red', 'volcano', 'orange', 'gold',
-		'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple',
-	],
-	tags: [
-		{
-			id: 'javascript',
-			name: 'javascript',
-			description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
-			color: '#f0da50',
-		},
-		{
-			id: 'nodejs',
-			name: 'Node.js',
-			description: 'Node.js is a tool for executing JavaScript in a variety of environments.',
-			color: '#90c53f',
-		},
-		{
-			id: 'rxjs',
-			name: 'RxJS',
-			description: 'RxJS is a library for reactive programming using Observables, for asynchronous operations.',
-			color: '#eb428e',
-		},
-		{
-			id: 'typescript',
-			name: 'typescript',
-			description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
-			color: '#257acc',
-		},
-		{
-			id: 'reactjs',
-			name: 'reactjs',
-			description: 'React is an open source JavaScript library used for designing user interfaces.',
-			color: '#61dbfa',
-		},
-		{
-			id: 'gatsby',
-			name: 'Gatsby.js',
-			description: 'A framework built over ReactJS to generate static page web application.  ',
-			color: '#6f309f',
-		},
-		{
-			id: 'html',
-			name: 'HTML',
-			description: 'A markup language that powers the web. All websites use HTML for structuring the content.',
-			color: '#dd3431',
-		},
-		{
-			id: 'css',
-			name: 'css',
-			description: 'CSS is used to style the HTML element and to give a very fancy look for the web application.',
-			color: '#43ace0',
-		},
-		{
-			id: 'python',
-			name: 'python',
-			description: 'A general purpose programming language that is widely used for developing various applications.',
-			color: '#f9c646',
-		},
-
-	],
-	defaultLanguage: 'en',
-	contactFormUrl: process.env.CONTACT_FORM_ENDPOINT
-		|| 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
-	googleAnalyticTrackingId: process.env.GA_TRACKING_ID || 'G-734WCNHH4E',
-	disqusScript: process.env.DISQUS_SCRIPT
-		|| 'https://tc-imba.disqus.com/embed.js',
-	wakatime: {
-		username: 'sleepymalc',
-		activity: 'eb201619-dfe8-485e-b683-11ce3475ad61',
-		language: 'cccb943f-2bfc-47e5-b218-c56b858c1a90',
-	},
+      date: 'Sept 2008 - June 2015',
+      icon: 'school',
+      title: 'Middle School',
+      location: 'Some Middle School, Somewhere',
+    }],
+  interests: [
+    {
+      icon: 'cubes',
+      title: 'Distributed Systems',
+    }, {
+      icon: 'layer-group',
+      title: 'Full Stack Development',
+    }, {
+      icon: ['fab', 'linux'],
+      // icon: 'linux',
+      title: 'Open Source Community',
+    }],
+  experience: [
+    {
+      title: 'Work',
+      position: 'left',
+      data: [
+        {
+          date: 'Aug 2019 - Present',
+          title: 'Software Engineer',
+          location: 'Somewhere',
+          description: 'description',
+        },
+      ],
+    }, {
+      title: 'Teaching',
+      position: 'right',
+      data: [
+        {
+          date: 'Aug 2019',
+          title: 'Teaching Assistant of XXX',
+          location: 'Somewhere',
+        }, {
+          date: 'Aug 2018',
+          title: 'Teaching Assistant of XXX',
+          location: 'Somewhere',
+        }, {
+          date: 'Aug 2017',
+          title: 'Teaching Assistant of XXX',
+          location: 'Somewhere',
+        }, {
+          date: 'Aug 2018',
+          title: 'Teaching Assistant of XXX',
+          location: 'Somewhere',
+        }, {
+          date: 'Aug 2017',
+          title: 'Teaching Assistant of XXX',
+          location: 'Somewhere',
+        },
+      ],
+    }, {
+      title: 'Volunteer',
+      position: 'left',
+      data: [
+        {
+          date: 'Aug 2019',
+          title: '[fxh](https://github.com/Reapor-Yurnero) home visit third time',
+          location: 'Somewhere',
+        }, {
+          date: 'Aug 2018',
+          title: '[fxh](https://github.com/Reapor-Yurnero) home visit second time',
+          location: 'Somewhere',
+        }, {
+          date: 'Aug 2017',
+          title: '[fxh](https://github.com/Reapor-Yurnero) home visit\n\n111',
+          location: 'Somewhere',
+        },
+      ],
+    },
+  ],
+  awards: [
+    {
+      date: 'Someday',
+      title: 'Some Award',
+    }, {
+      date: 'Someday',
+      title: 'Another Award',
+    },
+  ],
+  tagColors: [
+    'magenta', 'red', 'volcano', 'orange', 'gold',
+    'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple',
+  ],
+  tags: [
+    {
+      id: 'javascript',
+      name: 'javascript',
+      description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.',
+      color: '#f0da50',
+    },
+    {
+      id: 'nodejs',
+      name: 'Node.js',
+      description: 'Node.js is a tool for executing JavaScript in a variety of environments.',
+      color: '#90c53f',
+    },
+    {
+      id: 'rxjs',
+      name: 'RxJS',
+      description: 'RxJS is a library for reactive programming using Observables, for asynchronous operations.',
+      color: '#eb428e',
+    },
+    {
+      id: 'typescript',
+      name: 'typescript',
+      description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
+      color: '#257acc',
+    },
+    {
+      id: 'reactjs',
+      name: 'reactjs',
+      description: 'React is an open source JavaScript library used for designing user interfaces.',
+      color: '#61dbfa',
+    },
+    {
+      id: 'gatsby',
+      name: 'Gatsby.js',
+      description: 'A framework built over ReactJS to generate static page web application.  ',
+      color: '#6f309f',
+    },
+    {
+      id: 'html',
+      name: 'HTML',
+      description: 'A markup language that powers the web. All websites use HTML for structuring the content.',
+      color: '#dd3431',
+    },
+    {
+      id: 'css',
+      name: 'css',
+      description: 'CSS is used to style the HTML element and to give a very fancy look for the web application.',
+      color: '#43ace0',
+    },
+    {
+      id: 'python',
+      name: 'python',
+      description: 'A general purpose programming language that is widely used for developing various applications.',
+      color: '#f9c646',
+    },
+  ],
 };
